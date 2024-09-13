c8d2185a35f96d175b563ab03f873f0c0390fe38
Tom-on64 <kubecekt@gmail.com>
2024-09-13 11:46:40 +0200

    Improved output.s header and added input.s header. Got some basic I/O working

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,18 +1,42 @@
 bits 16
 
-%include "kernel/output.s"
-
-section .text
+;; This will be put at the start of the binary
 section entry
+    jmp _start
+
+;; Header files
+%include "kernel/output.s"
+%include "kernel/input.s"
+
+;; Main
+section .text
 _start:
     call cls
-    mov si, msg
-    call puts
+    print "Welcome to MY-DOS!",ENDL
 
+.input:
+    call getc
+    cmp al, 0x0d
+    je .newline
+    mov ah, [attr]
+    mov bx, [cursor]
+    call putc
+    inc word [cursor]
+    jmp .input
+
+.newline:
+    mov bx, [cursor]
+    call newline
+    mov [cursor], bx
+    jmp .input
+
+.halt:
     cli
-.halt: hlt
+    hlt
     jmp .halt
 
+;; Variables etc.
 section .data
-msg: db "Hello, World!", 0
+bufLen: db 0
+buffer: times 256 db 0
 
--- kernel/output.s
+++ kernel/output.s
@@ -1,10 +1,22 @@
 %ifndef OUTPUT_S
 %define OUTPUT_S
 
-%define ENDL 0x0a, 0x0d
+%define ENDL 0x0d
 %define VIDMEM 0xb800
 %define ROWS 25
 %define COLS 80
+
+%macro print 1+
+    section .data
+%%string:
+    db %1, 0
+    section .text
+    mov si, %%string
+    mov ah, [attr]
+    mov bx, [cursor]
+    call puts
+    mov [cursor], bx
+%endmacro
 
 section .text
 
@@ -12,46 +24,57 @@ section .text
 ;; Args:
 ;;  al - Character
 ;;  ah - Color attribute
-;;  bx - Offset into video memory
+;;  bx - Cursor position
 putc:
-    pusha
+    push ax
+    push cx
+    push bx
     push es
 
     ; Can't modify es directly so i need to use cx
     mov cx, VIDMEM
     mov es, cx
 
-    imul bx, 2
+    imul bx, 2  ; 1 character takes 2 bytes (ASCII byte, Attribute byte)
     
     ; Write character into video memory
     mov [es:bx], ax
 .done:
     pop es
-    popa
+    pop bx
+    pop cx
+    pop ax
     ret
 
 ;; Prints a string to the screen
 ;; Args:
 ;;  si - Pointer to string
+;;  ah - Color attribute
+;;  bx - Cursor position
+;; Rets:
+;;  bx - New cursor position
 puts:
-    pusha
-    mov bx, [cursor]
-    mov ah, [attr] ; White on black
+    push si
+    push ax
 .print:
     lodsb       ; mov al, [si]; inc si
-    or al, al   ; Check for end of string
-    jz .done
-
-    call putc
-    inc bx
-
+    or al, al   ; Check for null byte
+    jz .done    ; Null byte means end of string
+    cmp al, ENDL; Check if we have a newline
+    je .newline ; Newline
+    call putc   ; Print the character
+    inc bx      ; Move forward 1 char
+    jmp .print  ; Print next character
+.newline:
+    call newline
     jmp .print
 .done:
-    mov [cursor], bx
-    popa
+    pop ax
+    pop si
     ret
 
 ;; Clears the screen
+;; Args:
 cls:
     pusha
     push es
@@ -59,8 +82,8 @@ cls:
     mov ax, VIDMEM  ; Set es to the VIDMEM address
     mov es, ax
 
-    mov ah, [attr]  ; Clearing is just filling the screen with spaces
-    mov al, ' '
+    mov ah, [attr]
+    mov al, ' '     ; Clearing is just filling the screen with spaces
     xor bx, bx
     mov cx, (ROWS*COLS*2) ; Amount of bytes in the Video Buffer
 .loop:
@@ -70,12 +93,37 @@ cls:
     add bx, 2       ; Wrote 1 word = 2 bytes
     jmp .loop
 .done:
+    mov word [cursor], 0
     pop es
     popa
     ret
 
+;; Moves the cursor down a line
+;; Args:
+;;  bx - cursor
+;; Rets:
+;;  bx - new cursor
+newline:
+    push ax
+    push dx
+
+    ;; We need to calculate this equation: cursor += COLS - (cursor % COLS)
+    mov ax, bx  ; Store cursor in ax
+    push ax     ; Store that on the stack
+    mov dx, 0   ; Prepare dx for division
+    mov bx, COLS; Number to divide ax by
+    div bx      ; ax /= bx; dx = ax % bx; This gives us (cursor % COLS) in dx
+    mov bx, COLS; Prepare bx for subtraction
+    sub bx, dx  ; bx -= dx; This gives us COLS - dx
+    pop ax      ; Restore old cursor position from stack
+    add bx, ax  ; cursor += ax; ax is COLS - (cursor % COLS)
+
+    pop dx
+    pop ax
+    ret
+
 section .data
-attr: db 0x17
 cursor: dw 0
+attr: db 0x17
 
 %endif
