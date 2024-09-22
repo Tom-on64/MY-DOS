0a2ac696ded2cd72467620062296b5c960aaec71
Tom-on64 <kubecekt@gmail.com>
2024-09-22 15:05:16 +0200

    Improved disk.s and added diskWrite() implementation

--- inc/disk.s
+++ inc/disk.s
@@ -4,6 +4,24 @@
 ;; TODO: Don't use BIOS interrupts
 
 section .text
+
+;; Internal setup function
+;; Args:
+;;  ax - LBA address low
+;;  bx - LBA address high
+;;  cx - Sector count
+;;  di - Destination
+_setupPacket:
+    mov byte [DAPACK.packsize], 16  ; 16 bytes
+    mov byte [DAPACK.zero], 0       ; Always zero
+    mov word [DAPACK.seccount], cx  ; Sector count
+    mov word [DAPACK.destaddr], di  ; Destination address
+    mov word [DAPACK.destpage], 0   ; Destination page (Always zero)
+    mov word [DAPACK.lba_low], ax   ; Lba address low word
+    mov word [DAPACK.lba_hi], ax    ; Lba address high word
+    mov dword [DAPACK.lba_big], 0   ; Extra LBA bytes (Unused)
+    ret
+
 ;; Reads sectors from the disk
 ;; Args:
 ;;  ax - LBA address low
@@ -19,13 +37,9 @@ diskRead:
     push es
 
     xor si, si
-    mov es, si      ; Clear es
-    mov si, DAPACK  ; Disk Address Packet
-
-    mov word [DAPACK.d_lba], ax     ; Lba address low word
-    mov word [DAPACK.d_lba+2], ax   ; Lba address high word
-    mov word [DAPACK.blkcount], cx  ; Sector count
-    mov word [DAPACK.destaddr], di  ; Destination address
+    mov es, si          ; Clear es
+    mov si, DAPACK      ; Disk Address Packet
+    call _setupPacket   ; Setup the packet with correct values
     mov ah, 0x42        ; Read sectors (Extension)
     int 0x13
 
@@ -35,17 +49,40 @@ diskRead:
     ret
 
 ;; Writes sectors to the disk
-;; TODO:
-diskWrite: ret
+;; Args:
+;;  ax - LBA address low
+;;  bx - LBA address high
+;;  cx - Sector count
+;;  dl - Drive number
+;;  di - Destination
+;; Rets:
+;;  CF = 1 - Write failed
+diskWrite:
+    push ax
+    push si
+    push es
+
+    xor si, si
+    mov es, si          ; Clear es
+    mov si, DAPACK      ; Disk Address Packet
+    call _setupPacket   ; Setup the packet with correct values
+    mov ah, 0x43        ; Write sectors (Extension)
+    int 0x13
+
+    pop es
+    pop si
+    pop ax
+    ret
 
 section .data
 DAPACK:         ; Disk Address Packet
-    db 16       ; Packet size in bytes
-    db 0        ; Always zero
-.blkcount: dw 0 ; Block/sector count (Some BIOSes may limit this to a max of 127)
+.packsize: db 16; Packet size in bytes
+.zero:     db 0 ; Always zero
+.seccount: dw 0 ; Sector count (Some BIOSes may limit this to a max of 127)
 .destaddr: dw 0 ; Destination
-    dw 0       ; Memory page
-.d_lba: dd 0    ; LBA Address
-    dd 0        ; More bytes only for BIG LBA's (>4B)
+.destpage: dw 0 ; Memory page
+.lba_low:  dw 0 ; LBA Address low word
+.lba_hi:   dw 0 ;             high word
+.lba_big:  dd 0 ; More bytes only for BIG LBA's (>4B)
 
 %endif
